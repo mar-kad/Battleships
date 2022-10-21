591eb6712fa68367683e0acf43a59d800c5c5a7d
KD <karldavis@me.com>
2022-10-21 11:08:28 +0100

    refactor pep8

--- manu_system.py
+++ manu_system.py
@@ -14,10 +14,12 @@ def title():
 def how_to_play():
     default_boats = ["5: Carrier", "4: Battleship", "3: Cruiser x 2", "2: Submarine"]
 
-    print("\nWelcome to battleboats!\n")
+    print("\nWelcome to Battleboats!\n")
     display_grid(generate_grid())
     print(
-        f"Each player hides ships on a grid containing vertical and horizontal space coordinates. \nPlayers take turns typing out row and column coordinates on the other player's grid in an attempt to identify a location that contains a ship.\n")
+        f"Each player hides ships on a grid containing vertical and horizontal space coordinates. \nPlayers take "
+        f"turns typing out row and column coordinates on the other player's grid in an attempt to identify a location "
+        f"that contains a ship.\n")
     print("Each player receives a game board and five ships of varying sizes.  The five ships are: ")
     for i in default_boats:
         print(i)
