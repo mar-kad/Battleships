d48f45544dc4765c05ba4babd41219067c1e98d7
KD <karldavis@me.com>
2022-10-21 11:53:00 +0100

    Added full boat options and check if save has been saved.

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from generators import generate_grid
 from set_boats import set_boats
 from menu_system import title, menu, how_to_play
 from play_game import play_loop
-from file_handlers import save_game, load_game
+from file_handlers import save_game, load_game, check_save_data
 
 rounds = 0
-boat_sizes = [2]
+boat_sizes = [5, 4, 3, 3, 2]
 
 title()
 how_to_play()
@@ -21,8 +21,12 @@ while True:
         play_loop(p1_boats, p1_attack, p2_boats, p2_attack, rounds, boat_sizes)
 
     elif choice == 2:
-        p1_boats, p1_attack, p2_boats, p2_attack, boat_sizes, rounds = load_game()
-        play_loop(p1_boats, p1_attack, p2_boats, p2_attack, rounds, boat_sizes)
+        is_saved = check_save_data()
+        if is_saved:
+            p1_boats, p1_attack, p2_boats, p2_attack, boat_sizes, rounds = load_game()
+            play_loop(p1_boats, p1_attack, p2_boats, p2_attack, rounds, boat_sizes)
+        else:
+            print("No save data found.")
 
     elif choice == 3:
         quit()
@@ -34,10 +38,3 @@ while True:
         p2_boats = set_boats(generate_grid(), boat_sizes)
         p2_attack = generate_grid()
         save_game(p1_boats, p1_attack, p2_boats, p2_attack, boat_sizes, rounds)
-
-
-
-
-
-
-
